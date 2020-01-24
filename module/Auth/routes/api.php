0db7c65975fc0554e01e445cfc7265a415762836
anggagewor <anggagewor@gmail.com>
2020-01-24 15:09:25 +0700

    add route get clients

--- module/Auth/routes/api.php
+++ module/Auth/routes/api.php
@@ -1,14 +1,15 @@
 <?php
-Route::prefix('v1')->namespace('Api')->group( function (){
+Route::prefix('v1')->namespace('Api')->group(function () {
     Route::get('/', 'AuthController@index');
-    Route::prefix('auth')->group(function(){
+    Route::prefix('auth')->group(function () {
         Route::get('/', 'AuthController@index');
         Route::post('/login', 'AuthController@login');
         Route::post('/register', 'AuthController@register');
         Route::post('/authorize', 'AuthController@authorize');
         Route::get('/token', 'AuthController@token');
     });
-    Route::prefix('clients')->group(function(){
+    Route::prefix('clients')->group(function () {
+        Route::get('/', 'ClientController@index');
         Route::post('/', 'ClientController@store');
     });
 });
